957392c55b8a67ec751ac1fc3b0dde20dd73b2b3
Alex <WraithOne@Dark-Abyss.net>
2017-04-22 10:17:33 +0200

    Basic Framework

---
+++ main.lua
@@ -0,0 +1,21 @@
+-- -----------------------------------------------------------------------------------
+-- Ludum Dare 38 "A Small World"
+--
+-- An Game by @WraithOne
+-- -----------------------------------------------------------------------------------
+
+local composer = require( "composer" )
+
+-- Hide status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- Seed the random number generator
+math.randomseed( os.time() )
+
+-- Reserve channel 1 for background music
+audio.reserveChannels( 1 )
+-- Reduce the overall volume of the channel
+audio.setVolume( 0.5, { channel=1 } )
+
+-- Go to the menu screen
+composer.gotoScene( "menu" )
